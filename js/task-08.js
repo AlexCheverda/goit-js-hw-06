cec3a451f4dd8f669f228cc847ea81c66d83737a
Aleksandr Cheverda <alexar@ua.fm>
2022-08-20 15:14:54 +0300

    add task 7 & 8
    додано варіанти вирішення завдань 7 та 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === '' || password === '') {
+        return alert('Заповніть всі поля!')
+    }
+
+    console.log({ email: email, password: password });
+
+    console.log(' ');
+    form.reset();
+}
